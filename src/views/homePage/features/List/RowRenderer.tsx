6462543f3f95d97d9b58c5235a865f0762bb085a
早起喝燕麦 <zhuowangyi@163.com>
2024-12-17 16:34:11 +0800

    list

--- src/views/homePage/features/List/RowRenderer.tsx
+++ src/views/homePage/features/List/RowRenderer.tsx
@@ -1,12 +1,8 @@
+import React from 'react';
+import WaterFallCard, {RecyclerNFT} from '../WaterFallCard';
 
-
-function RowRenderer(){
-
-
-    return null
+function RowRenderer(type: number | string, data: RecyclerNFT) {
+  return <WaterFallCard row={data as RecyclerNFT} />;
 }
-
-
-
 
 export default RowRenderer;
--- src/views/homePage/features/List/index.tsx
+++ src/views/homePage/features/List/index.tsx
@@ -1,41 +1,95 @@
 import React from 'react';
-import {View, Text, StyleSheet, Dimensions} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 
-import {RecyclerListView, DataProvider, LayoutProvider} from 'recyclerlistview';
-const {width} = Dimensions.get('window');
+import {
+  RecyclerListView,
+  DataProvider,
+  BaseLayoutProvider,
+} from 'recyclerlistview';
+import {WaterfallLayoutProvider} from '../../components/RecyclerListView/WaterfallLayoutProvider';
+import {useInfiniteQuery} from 'react-query';
+import {
+  queryRecyclerAnimals,
+  RecyclerNFTs,
+  RecyclerNFT,
+  recyclerQueryOption,
+} from '../WaterFallCard';
+import {animalsUrl} from '../../api/homeAPI';
+import RowRenderer from './RowRenderer';
+
 // RLV 的模板代码
 
 function List() {
+  const {
+    status,
+    data: recyclerNFTs,
+    isFetchingNextPage,
+    fetchNextPage,
+    hasNextPage,
+  } = useInfiniteQuery<RecyclerNFTs>(
+    animalsUrl,
+    queryRecyclerAnimals,
+    recyclerQueryOption,
+  );
+
+  // 格式处理
+  const nfts = recyclerNFTs?.pages.reduce<RecyclerNFT[]>(
+    (accumulator, page) => {
+      return accumulator.concat(
+        page.items.reduce<RecyclerNFT[]>((all, group) => all.concat(group), []),
+      );
+    },
+    [],
+  );
+
+  // 创建数据提供者
+  let dp = new DataProvider((r1, r2) => r1.id !== r2.id);
+  // 都请求回来时才渲染
+  if (nfts) {
+    dp = dp.cloneWithRows([...nfts]);
+  }
   // 布局提供者
-  const layoutProvider = new LayoutProvider(
-    () => 0, // 返回一个固定的布局类型 0
-    (type, dim) => {
-      dim.width = width; // 适应屏幕宽度
-      dim.height = 50; // 设置每个 item 的高度
+  // @ts-ignore
+  const layoutProvider: BaseLayoutProvider = new WaterfallLayoutProvider(
+    index => dp.getDataForIndex(index).type,
+    (type, dim, index) => {
+      dim.width = dp.getDataForIndex(index).width;
+      dim.height = dp.getDataForIndex(index).height;
     },
   );
-  // 创建数据提供者
-  const dataProvider = new DataProvider((r1, r2) => r1.id !== r2.id);
-  // 示例数据
-  const data = Array.from({length: 1000}, (_, index) => ({
-    id: index.toString(),
-    title: `Item ${index + 1}`,
-  }));
-  // 渲染每个 item 的函数
-  const rowRenderer = (type: any, data: any) => {
-    return (
-      <View style={styles.item}>
-        <Text style={styles.text}>{data.title}</Text>
-      </View>
-    );
-  };
+
+  const Footer = () => (
+    <Text style={styles.footer}>
+      {isFetchingNextPage
+        ? '加载中...'
+        : hasNextPage
+        ? '加载中...'
+        : '没有数据了'}
+    </Text>
+  );
+
+  if (status === 'error') {
+    return <Text>你的服务跑丢了</Text>;
+  }
+
+  if (status === 'loading') {
+    return <Text>正在加载中...</Text>;
+  }
+
   return (
-    <RecyclerListView
-      style={{flex: 1}}
-      dataProvider={dataProvider.cloneWithRows(data)} // 使用 cloneWithRows 来加载数据
-      layoutProvider={layoutProvider}
-      rowRenderer={rowRenderer} // 渲染函数
-    />
+    <View style={styles.container}>
+      {dp.getSize() > 0 ? (
+        <RecyclerListView
+          onEndReached={() =>
+            status === 'success' && !isFetchingNextPage && fetchNextPage()
+          }
+          dataProvider={dp}
+          layoutProvider={layoutProvider}
+          rowRenderer={RowRenderer}
+          renderFooter={Footer}
+        />
+      ) : null}
+    </View>
   );
 }
 
@@ -53,7 +107,7 @@ const styles = StyleSheet.create({
   },
   container: {flex: 1, backgroundColor: '#FAFAFA'},
   liked: {fontSize: 12},
-  footer: {height: 50},
+  footer: {height: 50, textAlign: 'center'},
 });
 
 export default List;
